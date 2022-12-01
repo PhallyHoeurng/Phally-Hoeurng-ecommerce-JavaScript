9ac97789bfbed2cfebfc9dbfccadddb7d7e6b947
PhallyHoeurng <phallyhoeurng9@gmail.com>
2022-12-02 00:36:24 +0700

    [SELLER.JS] display add new prodcut

--- static/js/seller.js
+++ static/js/seller.js
@@ -1,152 +1,155 @@
-let products = [
-    {id : 1 ,name: 'គន្លឹះរៀនពូកែ', price: "$  5.00", img: 'static/imges/speaking.jpg', rate: 4},
-    
-    {id : 2 ,name: "Charlotte's", price: "$  5.00", img: 'static/imges/charlott.jpg', rate: 5},
-    
-    {id : 3 ,name: 'The Jungle book', price: "$  8.00", img: 'static/imges/JUNGLE.jpg', rate: 3},
-     
-    {id : 4 ,name: 'JOHN GRISHAM', price: "$  30.00", img: 'static/imges/JOHN.jpg', rate: 3},
-    
-    {id : 5 ,name: 'Nancy Drew', price: "$  22.00", img: 'static/imges/nANCY.JPG', rate: 2},
-    
-    {id : 6 ,name: 'JUNGLE', price: "$  6.00", img: 'static/imges/jungle.webp', rate: 4},
-    
-    {id : 7 ,name: 'JOHN GRISHAM', price: "$  5.00", img: 'static/imges/JOHN.jpg', rate: 5},
-    
-    {id : 8 ,name: 'STORY BOOK', price: "$   10.22", img: 'static/imges/STORY%20BOOK.jpg', rate: 3},
-     
-    {id : 9 ,name: 'ស្គាល់សេ្នហាទេ', price: "$  20.22", img: 'static/imges/knowlove.jpg', rate: 5},
-    
-    {id : 10 ,name: 'គន្លឹះរៀនពូកែ', price: "$  19.00", img: 'static/imges/study.jpg', rate: 5},
-    
-    {id : 11 ,name: 'លក់អារម្មណ៏', price: "$  5.50", img: 'static/imges/felling.jpg', rate: 5},
-    
-    {id : 12 ,name: 'សារភាពស្នេហា', price: "$  33.00", img: 'static/imges/love.jpg', rate: 5},
-]
+let containt5 = document.getElementById('contain5');
+let form_field = document.getElementById('dialog-info');
+let  create_Book = document.querySelector("#create");
 
 
-// function -----------------
+// display dilog
 
-function hide(element){
-    // todo 
-}
 function show(element){
-    // todo 
+    element.style.display = "block"
+}
+
+function onAddBook(){
+    show(form_field);
+}
+//btnCencel
+function hide(element){
+    element.style.display = "none"
+}
+
+function cencelBook(){
+    hide(form_field);
+}
+
+//data prodcut
+
+let products = [
+    {name: 'វិធីនិយាយ', price:5.25, img: 'img/speaking.jpg', rate: 4},
     
-}
-
-function loadData(){
-    // todo 
-}
-
-function saveData(){
-    // todo 
-}
-function displayData(){
-
-
-
-
-}
-console.log(localStorage.getItem("hello"))
-
-
-
-
-let containt5 = document.getElementById('contain5')
-let form_field = document.getElementById('form-field')
-
-
-const listthead = [
-    { id: "ID", product: "BOOK", price: "PRICES", status: "DELETE" }
+    {name: "Charlotte's", price:5.55, img: 'img/charlott.jpg', rate: 5},
+    
+    {name: 'The Jungle book', price:8.25, img: 'img/JUNGLE.jpg', rate: 3},
+     
+    {name: 'JOHN GRISHAM', price:30.55, img: 'img/JOHN.jpg', rate: 3},
+    
+    {name: 'Nancy Drew', price:22.77, img: 'img/nANCY.JPG', rate: 2},
+    
+    {name: 'JUNGLE', price:6.99, img: 'img/jungle.webp', rate: 4},
+    
+    {name: 'JOHN GRISHAM', price:5.44, img: 'img/JOHN.jpg', rate: 5},
+    
+    {name: 'STORY BOOK', price:10.25, img: 'img/STORY%20BOOK.jpg', rate: 3},
+     
+    {name: 'ស្គាល់សេ្នហាទេ', price:20.25, img: 'img/knowlove.jpg', rate: 5},
+    
+    {name: 'គន្លឹះរៀនពូកែ', price:19.99, img: 'img/study.jpg', rate: 5},
+    
+    {name: 'លក់អារម្មណ៏', price:5.55, img: 'img/felling.jpg', rate: 5},
+    
+    {name: 'សារភាពស្នេហា', price:50.09, img: 'img/love.jpg', rate: 5},
 ]
+
+//create thead
+const listthead = [
+    {product: "BOOK", img:"IMAGE" ,price: "PRICES", status:"EDIT OR DELETE"}
+]
+
+//localstorage 
+function saveBook(){
+    localStorage.setItem("products",JSON.stringify(products))
+}
+function loadBook(){
+    const storage = JSON.parse(localStorage.getItem("product"))
+    if(storage.lenght > 0){
+        products = storage 
+    }
+    else{
+        localStorage.removeItem("product")
+    }
+}
+
+
+///create tble 
 
 function table() {
     let table = document.createElement('table');
     let thead = document.createElement('thead');
     for (let value of listthead) {
-        let tr = document.createElement('tr')
+        let tr = document.createElement('tr');
         let th = document.createElement("th");
         let th1 = document.createElement("th");
         let th2 = document.createElement("th");
         let th3 = document.createElement("th");
 
-        th.textContent = value.id;
-        th1.textContent = value.product;
-        th2.textContent = value.price;
+        th.textContent = value.product;
+        th1.textContent = value.img;
+        th2.textContent = value.price ;
         th3.textContent = value.status;
 
-        tr.appendChild(th)
-        tr.appendChild(th1)
-        tr.appendChild(th2)
-        tr.appendChild(th3)
-        thead.appendChild(tr)
-        table.appendChild(thead)
-
+        tr.appendChild(th);
+        tr.appendChild(th1);
+        tr.appendChild(th2);
+        tr.appendChild(th3);
+        thead.appendChild(tr);
+        table.appendChild(thead);
     }
 
     for (let value of products) {
-        let tr = document.createElement('tr')
+
+        let tr = document.createElement('tr');
         let td = document.createElement("td");
         let td1 = document.createElement("td");
         let td2 = document.createElement("td");
         let td3 = document.createElement("td");
-        let i = document.createElement("i");
 
-        i.className = "fa fa-fw fa-trash"
-        i.classList.add("trash");
-        td.textContent = value.id;
-        td1.textContent = value.name;
-        td2.textContent = value.price;
-        
-        td3.appendChild(i);
-        tr.appendChild(td)
-        tr.appendChild(td1)
+        let li1 = document.createElement("i");
+        li1.className = "fa fa-fw fa-trash";
+        li1.classList.add("trash");
+
+        let li2 = document.createElement("i");
+        li2.className = "fa fa-fw fa-edit";
+        li2.classList.add("edit");
+
+        let img = document.createElement("img");
+        img.className = "imgTable";
+        img.src = value.img;
+    
+
+        td.textContent = value.name;
+        td2.textContent = value.price + " $ ";
+
+        td3.appendChild(li1)
+        td3.appendChild(li2)
+        td1.appendChild(img)
+
+        tr.appendChild(td);
+        tr.appendChild(td1);
         tr.appendChild(td2)
-        tr.appendChild(td3)
-        thead.appendChild(tr)
-        table.appendChild(thead)
+        tr.appendChild(td3);
+        thead.appendChild(tr);
+        table.appendChild(thead);
     }
     containt5.appendChild(table)
 }
 table()
 
+/// appand create new prodcut
+function toCreate (event){
+    let priceForm = document.querySelector("#price").value;
+    let imgForm = document.querySelector("#image").value;
+    let proForm = document.querySelector("#name-Product").value;
 
-function show(element){
-    element.style.display = "block"
-}
-function hide(element){
-    element.style.display = "none"
-}
-
-hide(form_field)
-
-function onAddBook(){
-    show(form_field)
-}
-
-buttonClick = document.getElementById("addBook")
-buttonClick.addEventListener('click',onAddBook);
-for (let book of buttonClick){
-    if ("click"){
-        table.push(buttonClick)
+    let newProdcut = {
+        name:proForm,
+        img : imgForm,
+        price: priceForm,
     }
-}
-console.log(table)
 
-
-function onAddData(event){
-    event.preventDefault()
-    let object ={};
-    show(form_field);
-    product = document.getElementById("name").value;
-    prices = document.getElementById("price").value;
-
-    object.name = products
-    object.price = prices
-    products.push(object)
+    products.push(newProdcut);
+    hide(form_field);
     console.log(products);
+    saveBook();
+    table();
+
 }
-onAddData()
-buttononAddData = document.getElementById("addData")
-buttononAddData.addEventListener('click',onAddData);
+create_Book.addEventListener("click", toCreate)
